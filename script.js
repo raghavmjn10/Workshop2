ee47e37049543ef0bde63a55c093f8ec34859f6b
raghavmjn10 <raghavmjn10@gmail.com>
2025-11-28 19:52:32 +0545

    Fixed sync issue

--- script.js
+++ script.js
@@ -1,18 +1,10 @@
-// MENU TOGGLE
-const menuBtn = document.getElementById("menu-btn");
-const navLinks = document.getElementById("nav-links");
-
-menuBtn.addEventListener("click", () => {
-    navLinks.classList.toggle("open");
-});
-
-// FORM MESSAGE
+// CONTACT FORM MESSAGE
 const form = document.getElementById("contact-form");
 const msg = document.getElementById("form-msg");
 
 form.addEventListener("submit", (e) => {
     e.preventDefault();
-    msg.textContent = "Message sent!";
-    msg.style.color = "lightgreen";
+    msg.textContent = "Your message has been sent!";
+    msg.style.color = "green";
     form.reset();
 });
